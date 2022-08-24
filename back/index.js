2ff39bb3366d5370dde202052d5dcf19b53bce3f
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-24 16:28:02 -0300

    feat: connection created mysql

--- back/index.js
+++ back/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mysql = require('mysql2');
+
+const cors = require('cors');
 
 const app = express();
 
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'tiago',
+  password: 'abranges',
+  database: 'crud_games',
+});
+
+app.use(cors());
+app.use(express.json());
+
 const PORT = 3001;
 app.get('/', (req, res) => {
-  res.status(200).json('funcinei');
+  const query = "INSERT INTO Games ( name, cost, category ) VALUES ('mk', 13, 'luta')";
+  db.query(query, (error, result) => {
+    console.log(error);
+  });
 });
 
 app.listen(PORT, () => console.log(`rodando na porta: ${PORT}`));
