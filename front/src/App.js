d57f01cd8438f721b4b91e66e89707603639df3d
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-24 13:59:35 -0300

    feat: handle input generic

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [values, setValues] = useState('');
+  console.log(values);
+
+  const handleChangeValues = ({ target }) => {
+    const { name, value } = target;
+    setValues((prevValue) => ({
+      ...prevValue,
+      [name]: value,
+    }));
+  };
   return (
     <div className="app-container">
       <div className="register-container">
-        <h1 className='register-title'>Scrim Shop</h1>
+        <h1 className="register-title">Scrim Shop</h1>
         <input
+          onChange={handleChangeValues}
           type="text"
           name="name"
           className="register-input"
           placeholder="Name"
         />
         <input
+          onChange={handleChangeValues}
           type="text"
           name="cost"
           className="register-input"
           placeholder="Cost"
         />
         <input
+          onChange={handleChangeValues}
           type="text"
           name="category"
           className="register-input"
