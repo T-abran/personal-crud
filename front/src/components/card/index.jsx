f57b02ccc6c3a582579c97ead8c3bf6aba8e6c85
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-25 17:22:24 -0300

    feat: created card

--- front/src/components/card/index.jsx
+++ front/src/components/card/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react'
+import './card.css'
+import FormDialog from '../dialog/dialog';
 
-function Card() {
+function Card({name, cost,category,setListCard, listCard,id}) {
+  const [open, setOpen] = useState(false);
   return ( 
-  <div>
-card
+    <>
+    <FormDialog open={open} setOpen={setOpen} name={name} cost={cost} category={category} listCard={listCard} id={id} setListCard={setListCard} />
+  
+  <div className='card-container' onClick={() => setOpen(true)}>
+<h1 className='card-title'> {name}</h1>
+<p className="card-id">{id}</p>
+<p className='card-category'>{category}</p>
+<p className='card-cost'>R$ {cost}</p>
   </div> 
+  </>
   );
 }
 
