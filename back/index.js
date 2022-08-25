02de9889cb58a4787e31c3207807e3b6d4ecd370
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-25 18:34:08 -0300

    feat: added environment variable

--- back/index.js
+++ back/index.js
@@ -1,21 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
-
 const cors = require('cors');
 
 const app = express();
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'tiago',
-  password: 'abranges',
-  database: 'crud_games',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE_NAME,
+  waitForConnections: process.env.MYSQL_WAIT_FOR_CONNECTIONS,
+  connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
+  queueLimit: process.env.MYSQL_QUEUE_LIMIT,
 });
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3001;
+const PORT = process.env.MYSQL_PORT || 3001;
 
 app.get('/getCards', (_req, res) => {
   let query = `SELECT * FROM Games`;
@@ -25,7 +29,7 @@ app.get('/getCards', (_req, res) => {
     } else res.send(result);
   });
 });
-
+console.log(process.env.MYSQL_PORT);
 app.post('/register', (req, res) => {
   const { name, cost, category } = req.body;
   let query = `INSERT INTO Games (name, cost, category ) VALUES (?, ?, ?)`;
