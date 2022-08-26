d139470543ba4a8aa06fabfc5e82273bf2890fe5
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-26 12:21:10 -0300

    feat: file sql

--- back/index.js
+++ back/index.js
@@ -6,20 +6,17 @@ const cors = require('cors');
 const app = express();
 
 const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE_NAME,
-  waitForConnections: process.env.MYSQL_WAIT_FOR_CONNECTIONS,
-  connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
-  queueLimit: process.env.MYSQL_QUEUE_LIMIT,
+  host:'localhost',
+  user: 'root', // seu usuario aqui
+  password: 'suasenha',// sua senha aqui
+  database: 'crud_games', // nome do banco
+
 });
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.MYSQL_PORT || 3001;
+const PORT =  3001;
 
 app.get('/getCards', (_req, res) => {
   let query = `SELECT * FROM Games`;
@@ -29,7 +26,7 @@ app.get('/getCards', (_req, res) => {
     } else res.send(result);
   });
 });
-console.log(process.env.MYSQL_PORT);
+
 app.post('/register', (req, res) => {
   const { name, cost, category } = req.body;
   let query = `INSERT INTO Games (name, cost, category ) VALUES (?, ?, ?)`;
